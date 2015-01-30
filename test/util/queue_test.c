5dfc4a2a03e8e27f1d5a2320d53b95efb6988ba9
Trevor Killeen <killeent@cs.washington.edu>
2015-01-29 20:54:07 -0800

    more unit tests for queue

--- test/util/queue_test.c
+++ test/util/queue_test.c
@@ -44,6 +44,44 @@ START_TEST(simple_enqueue_dequeue_test) {
 }
 END_TEST
 
+// test dequeue returns NULL from an empty queue
+START_TEST(dequeue_empty_queue_test)
+{
+  ck_assert_ptr_eq(dequeue(q), NULL);
+}
+END_TEST
+
+// tests queue maintains ordering
+START_TEST(queue_maintains_ordering_test)
+{
+  int *a, *b, *c;
+
+  a = (int *)malloc(sizeof(int));
+  ck_assert(a != NULL);
+  b = (int *)malloc(sizeof(int));
+  ck_assert(b != NULL);
+  c = (int *)malloc(sizeof(int));
+  ck_assert(c != NULL);
+  
+  // enqueue 3 items and check that they are dequeued
+  // in the correct order
+  ck_assert_int_eq(enqueue(q, (void *)a), 0);
+  ck_assert_int_eq(enqueue(q, (void *)b), 0);
+  ck_assert_int_eq(enqueue(q, (void *)c), 0);
+  ck_assert_ptr_eq((int *)dequeue(q), a);
+  ck_assert_ptr_eq((int *)dequeue(q), b);
+  ck_assert_ptr_eq((int *)dequeue(q), c);
+
+  free(a);
+  free(b);
+  free(c);
+
+}
+END_TEST
+
+// todo: add tests for interleaved ordering; freeing empty, single
+// and multi-element queues
+
 Suite *queue_suite() {
 	Suite *s;
 	TCase *tc_core;
@@ -55,6 +93,8 @@ Suite *queue_suite() {
 
   tcase_add_test(tc_core, allocation_test);
   tcase_add_test(tc_core, simple_enqueue_dequeue_test);
+  tcase_add_test(tc_core, dequeue_empty_queue_test);
+  tcase_add_test(tc_core, queue_maintains_ordering_test);
 
 	suite_add_tcase(s, tc_core);
 
