4e3a441699fef275c3df26f6a2e96cf65c43e178
Trevor Killeen <killeent@cs.washington.edu>
2015-02-12 17:28:49 -0800

    add test stubs for directory util test

--- test/util/directory_util_test.c
+++ test/util/directory_util_test.c
@@ -2,15 +2,111 @@
 //
 // Test Suite for the directory util implementation
 
+#include <stdlib.h>
+#include <assert.h>
+#include <check.h>
+
 #include "./directory_util_test.h"
+#include "../files/test_files.h"
 #include "../../src/util/queue.h"
 #include "../../src/util/directory_util.h"
 
-static void setup() {	
+struct queue *q;
+
+static void free_str(void *str) {
+	free(str);
 }
 
-static void teardown() {	
+static void setup() {	
+	q = allocate_queue();
+	assert(q != NULL);
 }
+
+static void teardown() {
+	free_queue(q, free_str);	
+}
+
+// tests trying to crawl a directory specified by an invalid path
+START_TEST(crawl_invalid_directory_test)
+{
+
+}
+END_TEST
+
+// tests trying to crawl a directory path that is actually a file
+START_TEST(crawl_file_directory_test)
+{
+
+}
+END_TEST
+
+// tests trying to crawl a directory we don't have permission to read
+START_TEST(crawl_directory_without_permission_test)
+{
+
+}
+END_TEST
+
+// tests crawling an empty directory
+START_TEST(crawl_empty_directory_test)
+{
+
+}
+END_TEST
+
+// tests crawling a directory with a single file
+START_TEST(crawl_single_file_directory_test)
+{
+
+}
+END_TEST
+
+// tests crawling a directory with a single file that we don't have
+// permission to read
+START_TEST(crawl_single_file_without_permission_directory_test)
+{
+
+}
+END_TEST
+
+// tests crawling a directory with multiple files
+START_TEST(crawl_multi_file_directory_test)
+{
+
+}
+END_TEST
+
+// tests crawling a directory with multiple files, where at least
+// one file we don't have permission to read
+START_TEST(crawl_multi_file_without_permission_directory_test)
+{
+
+}
+END_TEST
+
+// tests crawling a directory recursively, but the recursive directories
+// and the top-level directories contain no files
+START_TEST(crawl_empty_directory_recursive_test)
+{
+
+}
+END_TEST
+
+// tests crawling a directory recursively with a single file in
+// every directory
+START_TEST(crawl_single_file_directory_recursive_test)
+{
+
+}
+END_TEST
+
+// tests crawling a directory recursively with a multiple files in
+// every directory
+START_TEST(crawl_multi_file_directory_recursive_test)
+{
+
+}
+END_TEST
 
 Suite *directory_util_suite() {
 	Suite *s;
@@ -20,6 +116,18 @@ Suite *directory_util_suite() {
 
 	tc_core = tcase_create("core");
 	tcase_add_checked_fixture(tc_core, setup, teardown);
+
+	tcase_add_test(tc_core, crawl_invalid_directory_test);
+	tcase_add_test(tc_core, crawl_file_directory_test);
+	tcase_add_test(tc_core, crawl_directory_without_permission_test);
+	tcase_add_test(tc_core, crawl_empty_directory_test);
+	tcase_add_test(tc_core, crawl_single_file_directory_test);
+	tcase_add_test(tc_core, crawl_single_file_without_permission_directory_test);
+	tcase_add_test(tc_core, crawl_multi_file_directory_test);
+	tcase_add_test(tc_core, crawl_multi_file_without_permission_directory_test);
+	tcase_add_test(tc_core, crawl_empty_directory_recursive_test);
+	tcase_add_test(tc_core, crawl_single_file_directory_recursive_test);
+	tcase_add_test(tc_core, crawl_multi_file_directory_recursive_test);
 
 	suite_add_tcase(s, tc_core);
 
